8ba85731ad49565bdf762fd1c0163018d4df5bce
kruulik <karoliskrulis@gmail.com>
2018-10-20 15:24:50 -0400

    setup redux and configure eslint

---
+++ src/js/state/actions/user.actionTypes.js
@@ -0,0 +1,2 @@
+export const USER_GET = "USER_GET";
+export const USER_UPDATE = "USER_UPDATE";
---
+++ src/utils/userAPI.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import userData from '../assets/data/account.json';
+
+
+export const getUserData = async () => {
+
+
+};

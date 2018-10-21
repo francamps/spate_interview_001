a6c2e6fd4bc25159c4ca3b2e44eb3d576f8c0606
kruulik <karoliskrulis@gmail.com>
2018-10-21 01:03:35 -0400

    connect compoennt to state

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect, Provider } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import Account from '../views/Account'
+
+import store from '../../utils/app/store.js';
+
+import * as UserActions from '../state/actions/user.actions'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="app-wrapper">
-        <h1>Hi</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="app-wrapper">
+                    <Account />
+                </div>
+            </Provider>
+        );
+    }
 }
 
-export default App;
+export default App

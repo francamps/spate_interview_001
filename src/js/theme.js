f2da1da14f2a82aa1accae6846f2f090e45ec3ef
kruulik <karoliskrulis@gmail.com>
2018-10-21 01:02:17 -0400

    create theme variables file

---
+++ src/js/theme.js
@@ -0,0 +1,35 @@
+export const theme = {
+
+    // SPACING
+    space_xs: '8px',
+    space_sm: '16px',
+    space_md: '32px',
+    space_lg: '48px',
+    space_xl: '64px',
+
+    // COLORS
+    color_black: '#000',
+    color_primary: '#8445ff',
+    color_secondary: '#6212ff',
+    color_lightGray: '#bcbcbc',
+
+    // FONTS
+    font_family_base: 'Montserrat',
+    font_family_other: 'Merriweather',
+
+    font_size_sm: '8px',
+    font_size_md: '12px',
+    font_size_base: '14px',
+    font_size_h2: '18px',
+    font_size_h1: '32px',
+
+    font_weight_light: '200',
+    font_weight_base: '300',
+    font_weight_medium: '400',
+    font_weight_bold: '700',
+
+    font_spacing_wide: '0.2em',
+    font_spacing_base: '0.1em',
+    font_spacing_narrow: '-0.025em'
+
+}
